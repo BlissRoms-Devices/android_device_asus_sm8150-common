536aaac8f36e0484bfc2f07039ae9bd381c3a957
llololo <llololo@google.com>
2020-12-25 07:47:33 +0000

    sm8150-common: Remove A2DP input module in audio policy configuration
    No Pixel device support A2DP sink functionality.
    
    Bug: 163050296
    Test: A2DP related test.
    Change-Id: I269220cf29451ea79362fc004fa073713f72fdb2
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- msmnile.mk
+++ msmnile.mk
@@ -104,7 +104,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
 
 PRODUCT_COPY_FILES += \
-    frameworks/av/services/audiopolicy/config/a2dp_in_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_in_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
