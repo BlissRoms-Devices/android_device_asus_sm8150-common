7ed788d2962c8241d1e0822bcd03f25685418b05
micky387 <mickaelsaibi@free.fr>
2022-08-02 19:31:47 +0200

    sm8150-common: Build hidl memory block to vendor
    Needed for trustedui
    
    E vendor.qti.hardware.trustedui@1.0-service-qti: can't load library:libTrustedUITZ.so: dlopen failed: library "android.hidl.memory.block@1.0.so" not found: needed by /vendor/lib64/vendor.qti.hardware.trustedui@1.1.so in namespace (default)
    
    Change-Id: Ia8cd2426449242b3de66008e2fb384c63fa79f04
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- msmnile.mk
+++ msmnile.mk
@@ -525,6 +525,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.asus_msmnile
 
+# TrustedUI
+PRODUCT_PACKAGES += \
+    android.hidl.memory.block@1.0.vendor
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.dual_role_usb
