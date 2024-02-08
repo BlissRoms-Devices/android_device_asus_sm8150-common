de5934dc134cf2e8b65d97eb1984fb82dce6c2ec
Bruno Martins <bgcngm@gmail.com>
2024-02-08 17:05:32 +0000

    sm8150-common: Use the common device compatibility matrix from QCOM
    Change-Id: I0c0f3ecddbe6d7b232cc0d13c78ca58094e4e720
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -193,7 +193,7 @@ BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
 
 # VINTF
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH_COMMON)/vintf/manifest.xml
-DEVICE_MATRIX_FILE += $(DEVICE_PATH_COMMON)/vintf/compatibility_matrix.xml
+DEVICE_MATRIX_FILE += hardware/qcom-caf/common/compatibility_matrix.xml
 ODM_MANIFEST_FILES := $(DEVICE_PATH_COMMON)/vintf/manifest-qva.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH_COMMON)/vintf/framework_compatibility_matrix.xml \
