f5265385b7dcab4efe601381972dfbbf8dfe7f64
Bruno Martins <bgcngm@gmail.com>
2024-05-13 06:53:43 -0400

    sm8150-common: Migrate to QTI health AIDL service
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- msmnile.mk
+++ msmnile.mk
@@ -276,8 +276,7 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-service \
+    android.hardware.health-service.qti \
     android.hardware.health@2.1.vendor
 
 # HIDL
